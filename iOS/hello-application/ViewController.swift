fa32fb0b0a61ecf44ab05b23151048714a5b9dfc
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2019-08-09 06:55:08 +0200

    iOS Application update.

--- iOS/hello-application/ViewController.swift
+++ iOS/hello-application/ViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import UIKit
+import HelloMessages
 
 class ViewController: UIViewController {
 
+   private lazy var stackView = UIStackView()
+   private lazy var button = UIButton()
+
    override func viewDidLoad() {
       super.viewDidLoad()
-      view.backgroundColor = .blue
+      setupUI()
    }
 
 }
 
+extension ViewController {
+
+   private func setupUI() {
+      view.backgroundColor = .blue
+      view.addSubview(stackView)
+
+      stackView.translatesAutoresizingMaskIntoConstraints = false
+      stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+      stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+      stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+      stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+      stackView.axis = .vertical
+
+      stackView.addArrangedSubview(button)
+      stackView.addArrangedSubview(UIView())
+
+      button.setTitle("Get Time", for: .normal)
+      button.addTarget(self, action: #selector(getTime), for: .touchUpInside)
+   }
+
+   @objc private func getTime() {
+      let time = sayHello()
+      button.setTitle("\(time)", for: .normal)
+   }
+}
