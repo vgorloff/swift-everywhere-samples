4d49d83adac16001af30ce411fa886afa2e83560
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2019-08-09 06:31:17 +0200

    Added iOS Project

---
+++ iOS/hello-application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello-application
+//
+//  Created by Vlad Gorlov on 09.08.19.
+//  Copyright © 2019 WaveLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
