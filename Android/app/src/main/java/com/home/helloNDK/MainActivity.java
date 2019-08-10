df3517e97a7e149762455bbc452940e4662d0f21
Vlad Gorloff <volodymyr.gorlov@gmail.com>
2019-08-10 14:38:04 +0200

    Updated sample code.

--- Android/app/src/main/java/com/home/helloNDK/MainActivity.java
+++ Android/app/src/main/java/com/home/helloNDK/MainActivity.java
@@ -3,6 +3,8 @@ package com.home.helloNDK;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.system.ErrnoException;
+import android.system.Os;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Os.setenv("CFFIXED_USER_HOME", this.getFilesDir().getPath(), true);
+        } catch (ErrnoException e) {
+            e.printStackTrace();
+        }
         super.onCreate(savedInstanceState);
         final Button myButton = new Button(this);
         myButton.setText("Press me");
--- Package/Sources/HelloJNICore/Exports.swift
+++ Package/Sources/HelloJNICore/Exports.swift
@@ -1,9 +1,106 @@
 import Foundation
 import NDKLog
+#if os(Android)
+import FoundationNetworking
+#endif
+
+let tester = URLTester()
 
 @_cdecl("Java_com_home_helloNDK_SwiftLib_sayHello")
 public func sayHello() -> Int {
    // fatalError()
-   AndroidLogger.info("~~~~ TEST MESSAGE ~~~~")
+   AndroidLogger.info("SA - SwiftCore: Works!")
+   probeDispatch()
+   probeOperation()
+   probeSerialization()
+   tester.test()
    return Int(Date().timeIntervalSince1970)
 }
+
+func probeDispatch() {
+   let sema = DispatchSemaphore(value: 0)
+
+   let queue = DispatchQueue(label: "queueName")
+   queue.async {
+      AndroidLogger.info("SA - DispatchQueue: Works!")
+      sema.signal()
+   }
+
+   if sema.wait(timeout: .now() + 10) == .timedOut {
+      AndroidLogger.info("SA - DispatchQueue: Timeout.")
+   }
+}
+
+private let opQueue = OperationQueue()
+
+func probeOperation() {
+   let op = BlockOperation {
+      AndroidLogger.info("SA - BlockOperation: Works!")
+   }
+   opQueue.addOperations([op], waitUntilFinished: true)
+}
+
+
+func probeSerialization() {
+   let json = ["name": "SA - JSONSerialization/JSONDecoder: Works!"]
+   do {
+      let data = try JSONSerialization.data(withJSONObject: json, options: [])
+      struct Person: Decodable {
+         let name: String
+      }
+      let person = try JSONDecoder().decode(Person.self, from: data)
+      AndroidLogger.info(person.name)
+   } catch {
+      AndroidLogger.info(String(describing: error))
+   }
+}
+
+
+
+class URLTester: NSObject {
+
+
+   let config = URLSessionConfiguration.default
+   lazy var session = URLSession(configuration: config)
+
+   override init() {
+      super.init()
+//      session.delegate = self
+   }
+
+   func test() {
+      AndroidLogger.info("SA - URLSession: Seems not Working yet.")
+      if let url = URL(string: "https://www.google.com") {
+         let sema2 = DispatchSemaphore(value: 0)
+         let task = session.dataTask(with: url) { data, response, error in
+            if let response = response {
+               AndroidLogger.info("Response: " + String(describing: response))
+            }
+            if let error = error {
+               let nsError = error as NSError
+               AndroidLogger.info("Error: " + String(describing: nsError))
+               AndroidLogger.info("Error UserInfo: " + String(describing: nsError.userInfo))
+            }
+            if let data = data {
+               AndroidLogger.info("Data: " + String(describing: data))
+            }
+            sema2.signal()
+         }
+         task.resume()
+         if sema2.wait(timeout: .now() + 10) == .timedOut {
+            AndroidLogger.info("Timeout")
+         }
+         AndroidLogger.info("URL Task Completed")
+      } else {
+         AndroidLogger.info("bad url")
+      }
+   }
+}
+
+//extension URLTester: URLSessionDelegate {
+//
+//   func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge,
+//                   completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+//      AndroidLogger.info("Challenge: " + String(describing: challenge))
+//   }
+//}
